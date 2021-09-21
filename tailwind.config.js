73ab8b6a50e74d6a0298a1d31b6d842816c8605a
Piotr Godzień <piotrgodzien@otherlandlabs.com>
2021-09-21 16:57:43 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Blinker', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: '#F4120F',
+        secondary: '#222222',
+      },
+      backgroundImage: {
+        // 'herobanner': "url('https://otherlandlabs.com/_next/static/images/pattern-871dbd5e89966c7b04bf3ef0ebe03eed.svg')",
+        'herobanner': "url('http://verdepro.pl/dach.jpg')",
+        'footerbanner': "url('https://verdepro.pl/pattern.jpg')",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
