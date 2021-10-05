c0eaa2d5f00564aa9c2620581ead9df54840ef26
crstomatologia <piotrgodzien@otherlandlabs.com>
2021-10-05 17:05:25 +0200

    .

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,15 @@
 import 'tailwindcss/tailwind.css'
+import Typewriter from 'typewriter-effect/dist/core';
+// import Typewriter from 'typewriter-effect';
+
+new Typewriter("#typewriter-hero", {
+  strings: ["dachy stalowe", "dachy betonowe", "renowację dachów"],
+  autoStart: true,
+  loop: true,
+  delay: 75,
+  cursorClassName: "bg-clip-border font-normal",
+});
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
