be8ec0a3e0b20ead5b430bcd7d54479ef6f4c310
Piotr Godzień <piotrgodzien@otherlandlabs.com>
2021-10-05 17:47:36 +0200

    Revert "."
    This reverts commit e5b1edc27917061c17493b33e291eb57a7cfda27.

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,7 @@ module.exports = {
       },
       backgroundImage: {
         // 'herobanner': "url('https://otherlandlabs.com/_next/static/images/pattern-871dbd5e89966c7b04bf3ef0ebe03eed.svg')",
-        'herobanner': "url('http://verdepro.pl/b.jpg')",
+        'herobanner': "url('http://verdepro.pl/dach.jpg')",
         'footerbanner': "url('https://verdepro.pl/pattern_dark.jpg')",
         'midelbanner' : "url('http://verdepro.pl/dach4.jpg')",
       },
