6ebf28258583eda3df3a81dafe1590137578550b
Piotr Godzień <piotrgodzien@otherlandlabs.com>
2021-10-07 10:06:28 +0200

    mobile menu

--- components/_nav.jsx
+++ components/_nav.jsx
@@ -9,10 +9,10 @@ const Navigation = () => {
       );
     }
   }, []);
-
+  const [visible, setVisible] = useState(false);
   return (
 <>
-  <div className={`fixed bg-black top-0 z-40 py-6 mx-auto text-2xl w-full text-white ${small ? "small" : ""}`}>
+  <div className={`fixed bg-black top-0 z-50 py-6 mx-auto text-xl w-full text-white ${small ? "small" : ""}`}>
     <nav className="">
       <div className="flex items-center justify-between w-full px-4 mx-auto lg:px-20 ">
         <div className="h-auto w-36 lg:w-72">
@@ -31,15 +31,34 @@ const Navigation = () => {
           </div>
         </div>
         <div className="ml-auto lg:hidden">
+        <button className="focus:outline-none" onClick={() => setVisible(!visible)}>
           <svg className="p-3 border border-white " viewBox="0 0 100 80" fill="currentColor" width="50" height="50">
             <rect width="100" height="6"></rect>
             <rect y="30" width="100" height="6"></rect>
             <rect y="60" width="100" height="6"></rect>
           </svg>
+        </button>
         </div>
       </div>
     </nav>
   </div>
+{/* MOBILE */}
+<div className={`${
+            visible ? 'block' : 'hidden'
+          }  absolute inset-0 z-40 h-screen bg-black lg:hidden`}>
+  <div className="flex flex-col w-2/3 mx-auto space-y-3 text-3xl font-bold text-white mt-36">
+    <div className="">Oferta</div>
+    <div className="">O nas</div>
+    <div className="">Realizacje</div>
+    <div className="">Kariera</div>
+    <div className="">Kontakt</div>
+    <div className="flex space-x-4">
+      <img className="h-6 border border-white w-9" src="https://verdepro.pl/germany.png" alt="" />
+      <img className="h-6 border border-white w-9" src="https://verdepro.pl/nederland.png" alt="" />
+      <img className="h-6 border border-white w-9" src="https://verdepro.pl/united.png" alt="" />
+    </div>
+  </div>
+</div>
 </>
   );
 };
