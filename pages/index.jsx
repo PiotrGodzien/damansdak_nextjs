174774839437020f5fda9dacf9ef1e1526c738bc
crstomatologia <piotrgodzien@otherlandlabs.com>
2021-10-08 20:47:26 +0200

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -38,7 +38,7 @@ const Home = () => {
           <div className="relative z-30 my-auto">
             <div className="flex flex-col-reverse items-center justify-start px-8 mx-auto sm:w-4/5 lg:w-3/5 md:flex-row">
               <LazyShow>
-                <div className="items-center space-y-7 sm:space-y-8 lg:space-y-16">
+                <div className="items-center mt-16 2xl:mt-24 space-y-7 sm:space-y-8 lg:space-y-16">
                   <h1 className="text-4xl font-bold sm:text-5xl 2xl:text-8xl">Dachy płaskie w technologii membran PVC i TPO</h1>
                   <p className="text-xl sm:text-3xl">Niezależnie od projektu, nasz zespół profesjonalistów jest gotowy, aby urzeczywistnić Twoje plany</p>
                   <div className="lg:flex lg:space-x-8">
