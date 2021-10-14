0d95525ef2641f046bfa2c59e958243261c19c7c
crstomatologia <piotrgodzien@otherlandlabs.com>
2021-10-14 21:58:10 +0200

    Update oferta.jsx

--- pages/oferta.jsx
+++ pages/oferta.jsx
@@ -7,7 +7,7 @@ import Footer from '../components/_footer';
 import Cta from '../components/_cta';
 import CountUp from "react-countup";
 import VisibilitySensor from 'react-visibility-sensor';
-import Gallery from '../components/gallers/_gallery_lublin';
+// import Gallery from '../components/gallers/_gallery_lublin';
 
 
 const Home = () => {
@@ -64,7 +64,7 @@ const Home = () => {
           </div>
         </div>
       </section>
-      <Gallery />
+      {/* <Gallery /> */}
       <LazyShow>
         <Cta />
       </LazyShow>
