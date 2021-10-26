e68f766422be3d3025a0b8db4c5d1ef385c5cd3a
PGodzien <piotrgodzien@gmail.com>
2021-10-26 09:59:36 +0200

    Update _carousel.jsx

--- components/_carousel.jsx
+++ components/_carousel.jsx
@@ -5,15 +5,15 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 export default function CarouselComponent() {
   return (
     <div>
-      <Carousel infiniteLoop showThumbs={false} autoPlay>
+      <Carousel infiniteLoop showThumbs={false} transitionTime={1000} autoPlay>
         <div>
-          <img className="h-screen object-cover" src="https://damansdak.s3.eu-central-1.amazonaws.com/blonie_realizacja.jpeg" />
+          <img className="object-cover h-screen" src="https://damansdak.s3.eu-central-1.amazonaws.com/blonie_realizacja.jpeg" />
         </div>
         <div>
-          <img className="h-screen object-cover" src="https://damansdak.s3.eu-central-1.amazonaws.com/lublin_realizacja.jpeg" />
+          <img className="object-cover h-screen" src="https://damansdak.s3.eu-central-1.amazonaws.com/lublin_realizacja.jpeg" />
         </div>
         <div>
-          <img className="h-screen object-cover" src="https://damansdak.s3.eu-central-1.amazonaws.com/ruda_slaska_realizacja.jpeg" />
+          <img className="object-cover h-screen" src="https://damansdak.s3.eu-central-1.amazonaws.com/ruda_slaska_realizacja.jpeg" />
         </div>
       </Carousel>
     </div>
