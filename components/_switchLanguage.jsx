3f6490d16a3c1d4f5298f72899aee55706f6e68d
PGodzien <piotrgodzien@gmail.com>
2021-11-03 10:34:57 +0100

    Revert "translate"
    This reverts commit e9f28cd08f46a5df971d0a48ec22c30727ad6c5b.

--- components/_switchLanguage.jsx
+++ components/_switchLanguage.jsx
@@ -1,42 +1,29 @@
-import React from "react";
-import i18nConfig from "../i18n.json";
-import Link from "next/link";
+import React from 'react';
+import i18nConfig from '../i18n.json';
+import useTranslation from 'next-translate/useTranslation';
+import Link from 'next/link';
+import { useRouter } from 'next/router'
 
 const SwitchLanguage = () => {
-  const { locales } = i18nConfig;
+  const { locales, defaultLocale } = i18nConfig;
+  const { t, lang } = useTranslation('common');
+  const { asPath } = useRouter();
 
-  const codeToCountry = (code) => {
-    switch (code) {
-      case "de":
-        return "germany";
-
-      case "en":
-        return "united";
-
-      default:
-        return "poland";
-    }
-  };
   return (
-    <>
-      <div className="flex space-x-4">
-        {locales.map((lng) => {
+    <div>
+      <div id="dropdown-basic-button" title="Switch Lang">
+        {locales.map(lng => {
+          if (lng === lang) return null;
           return (
-            <Link href="" locale={lng} key={lng}>
-              {/* {t(`${flags}`)} */}
-              <img
-                alt=""
-                className="h-6 border border-white w-9"
-                src={`https://damansdak.s3.eu-central-1.amazonaws.com/${codeToCountry(
-                  lng
-                )}.png`}
-                layout="fill"
-              />
-            </Link>
+            <div key={lng}>
+              <Link href="/" locale={lng} key={lng}>
+                {t(`common:language-name-${lng}`)}
+              </Link>
+            </div>
           );
         })}
       </div>
-    </>
+    </div>
   );
 };
 
