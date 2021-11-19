e09c215557faab25359becec5def91bc7d62becb
Piotr Godzień <piotrgodzien@otherlandlabs.com>
2021-11-19 20:23:01 +0100

    about next-seo

--- pages/o-nas.jsx
+++ pages/o-nas.jsx
@@ -2,11 +2,44 @@ import Navigation from "../components/_nav";
 import Cta from "../components/_cta";
 import Footer from "../components/_footer";
 import useTranslation from "next-translate/useTranslation";
+import { NextSeo } from "next-seo";
 
 const Onas = () => {
   const { t, lang } = useTranslation("common");
   return (
     <>
+          <NextSeo
+        title={`Damansdak Polska - ${t("Onas")}`}
+        description='{t("aboutDescription")}'
+        canonical="https://damasndak/o-nas"
+        openGraph={{
+          url: "https://damansdak.pl/o-nas",
+          title: `Damansdak Polska – ${t("Onas")}`,
+          description: t("aboutDescription"),
+          images: [
+            {
+              url: "https://damansdak.mo.cloudinary.net/hero.webp",
+              width: 800,
+              height: 600,
+              alt: `Damansdak Polska – ${t("Onas")}`,
+            },
+            {
+              url: "https://damansdak.mo.cloudinary.net/hero.webp",
+              width: 900,
+              height: 800,
+              alt: `Damansdak Polska – ${t("Onas")}`,
+            },
+            { url: "https://damansdak.mo.cloudinary.net/hero.webp" },
+            { url: "https://damansdak.mo.cloudinary.net/hero.webp" },
+          ],
+          site_name: `Damansdak Polska – ${t("Onas")}`,
+        }}
+        twitter={{
+          handle: "@BDamans",
+          site: "@bdamans",
+          cardType: "summary_large_image",
+        }}
+      />
       <Navigation />
       <section className="lg:pt-20">
         <div className="relative flex items-center overflow-hidden text-center bg-cover py-36">
