f62df4bb72c4172a393ec1d80fae4c0acad9329a
PGodzien <piotrgodzien@gmail.com>
2021-11-23 18:30:49 +0100

    .

--- pages/wideo.jsx
+++ pages/wideo.jsx
@@ -7,22 +7,70 @@ const Oferta = () => {
   const { t, lang } = useTranslation("common");
   return (
     <>
+      {" "}
+      <NextSeo
+        title={`Damansdak Polska - ${t("Wideo")}`}
+        description={`${t("wideoDescription")}`}
+        canonical="https://damansdak.pl/wideo/"
+        languageAlternates={[
+          {
+            hrefLang: "de",
+            href: "https://damansdak.pl/de/wideo/",
+          },
+          {
+            hrefLang: "ru",
+            href: "https://damansdak.pl/ru/wideo/",
+          },
+          {
+            hrefLang: "en",
+            href: "https://damansdak.pl/en/wideo/",
+          },
+        ]}
+        openGraph={{
+          type: "website",
+          url: "https://damansdak.pl/wideo/ ",
+          title: `Damansdak Polska – ${t("Wideo")}`,
+          description: t("wideoDescription"),
+          images: [
+            {
+              url: "https://damansdak.mo.cloudinary.net/hero.webp",
+              width: 800,
+              height: 600,
+              alt: `Damansdak Polska – ${t("Wideo")}`,
+            },
+            {
+              url: "https://damansdak.mo.cloudinary.net/hero.webp",
+              width: 900,
+              height: 800,
+              alt: `Damansdak Polska – ${t("Wideo")}`,
+            },
+            { url: "https://damansdak.mo.cloudinary.net/hero.webp" },
+            { url: "https://damansdak.mo.cloudinary.net/hero.webp" },
+          ],
+          site_name: `Damansdak Polska – ${t("Wideo")}`,
+        }}
+        twitter={{
+          handle: "@BDamans",
+          site: "@bdamans",
+          cardType: "summary_large_image",
+        }}
+      />
       <Navigation />
       <section className="lg:pt-20">
         <div className="relative flex items-center overflow-hidden text-center bg-cover py-36">
           <img
             className="absolute object-cover w-full h-full bg-center"
             src="/wideo_slider.webp"
-            alt=""
+            alt="Obraz wideo"
           />
           <div className="absolute inset-0 bg-gradient-to-tr from-black via-black to-primary opacity-70"></div>
           <div className="relative max-w-4xl px-4 mx-auto">
             <p className="font-serif font-light text-center text-white uppercase text-md lg:text-left">
               {t("Wideo")}
             </p>
-            <h2 className="text-3xl text-center text-white lg:text-5xl lg:text-left font-heading">
+            <h1 className="text-3xl text-center text-white lg:text-5xl lg:text-left font-heading">
               {t("titleWideo")}
-            </h2>
+            </h1>
           </div>
         </div>
         <div className="py-4 mb-6 bg-pattern"></div>
