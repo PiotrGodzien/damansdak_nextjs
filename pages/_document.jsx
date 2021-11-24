779626832515a487ca970ec7192e0d2f7710d7ed
PGodzien <piotrgodzien@gmail.com>
2021-11-24 09:26:19 +0100

    GA

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import { GA_TRACKING_ID } from "../gtag";
 import Navigation from "../components/_nav";
 import Footer from "../components/_footer";
 import Cta from "../components/_cta";
@@ -10,6 +11,22 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
           <link rel="icon" href="/favicon.ico" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
@@ -17,7 +34,10 @@ class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css2?family=Blinker:wght@400;700&family=Lato:wght@300;400&display=swap"
           />
-          <meta name="google-site-verification" content="xXCEWAfdBPhefjp9dCqR96QizmSoh3V6Fub2yNrX-fA" />
+          <meta
+            name="google-site-verification"
+            content="xXCEWAfdBPhefjp9dCqR96QizmSoh3V6Fub2yNrX-fA"
+          />
         </Head>
         <body>
           <Main />
